e41b6b5ec4bd4c3d44e9fe5322d3490f1eabd189
Gonzales_edi_biotech <100884674+Edifgm21@users.noreply.github.com>
2022-11-23 21:07:54 -0500

    Edison Gonzales a contado las bacteriasa

--- bacterias.sh
+++ bacterias.sh
@@ -1,1 +1,8 @@
+echo "literal a"
+
+cut -d "," -f 2 grampa.csv | tail -n +2 | sort | uniq -c 
+echo "literal b"
+cut -d "," -f 2 grampa.csv | tail -n 2 | sort | uniq -c | sort -n -r | head -n 2 
+echo "literal c"
+cut -d "," -f 2 grampa.csv | tail -n 2 | sort | uniq -c | sort -n  | head -n 2
 
